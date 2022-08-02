ce527e6ecfda2bc6db88a252a9e64c9a7b1c343f
unknown <ivanalejandroherrera2000@gmail.com>
2022-08-01 22:53:24 -0300

    initial client configuration

--- server/index.js
+++ server/index.js
@@ -8,10 +8,18 @@ import { PORT } from './config.js';
 const app = express();
 //convierto el servidor express a htpp para que sea compatible con socket.io
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+  cors: {
+    origin: 'http://127.0.0.1:5173',
+  },
+});
 
 app.use(cors());
 app.use(morgan('dev'));
 
-app.listen(PORT);
+io.on('connection', (socket) => {
+  console.log(`user connected with id: '${socket.id}'`);
+});
+
+server.listen(PORT);
 console.log(`Server started on port ${PORT}`);
